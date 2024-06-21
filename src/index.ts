d565127473f93968a3d1abf485830d4e4897f48e
Mega Krash <jscattolini@gmail.com>
2024-06-21 21:46:34 +0200

    :tada:

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+
+const app = express();
+const port = process.env.APP_PORT || 5005;
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  "/pictures",
+  express.static(path.join(__dirname, "../public/pictures"))
+);
+
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur MegaS3!");
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server ready at port ${port} 🚀`);
+});
