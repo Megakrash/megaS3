56b5a5c1c48792a8e08708718700e78949a95e3b
Mega Krash <jscattolini@gmail.com>
2024-06-23 18:05:09 +0200

    :wrench: CORS

--- src/index.ts
+++ src/index.ts
@@ -17,22 +17,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
-
-app.options("*", cors(corsOptions));
-
 app.use(
   "/pictures",
   express.static(path.join(__dirname, "../public/pictures"))
